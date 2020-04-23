c01851f94de59515dbe4eba5c869be0cbce3f83d
Jason <jasonblue1626@gmail.com>
2020-04-23 04:18:14 -0500

    To fix the issues reading

--- src/data/SNIDDb.java
+++ src/data/SNIDDb.java
@@ -1,4 +1,4 @@
-package data;
+//package data;
 
 /**
  * testing
@@ -9,6 +9,7 @@ package data;
  */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class SNIDDb {
     // I
@@ -36,15 +37,79 @@ public class SNIDDb {
      * to check if there are any more lines in the file to read
      * @return A boolean value indicating if the next line in the file
      * can be read
+     * @throws IOException Which is later caught in the constructor for the
+     * SNIDDb
      */
 
     public boolean hasNext() throws IOException{
         return bReader.readLine() != null;
     }
-
+    /**
+     * Method to get the next line in the file
+     * 
+     */
     public String[] getNext() throws IOException{
-        String line = bReader.readLine();
-        return line.split(Character.toString(delimiter));
+        try{
+            String line = bReader.readLine();
+            return line.split(Character.toString(delimiter));
+        }catch(NullPointerException e){
+            System.out.println("The file is empty");
+            return new String[0];
+        }
     }
+
    
+   public void reWrite(){
+       try{
+            bReader.close();
+            bReader = new BufferedReader(new FileReader(fileName));
+        }catch(FileNotFoundException e){
+            System.out.println("The file could not be found");
+        }catch(IOException e){
+            System.out.println("An inexpected IOException has occured: " + e.getMessage());
+        }
+    
+   }
+   public void putNext(String[] data){
+       // Temporary bandaid that works
+        try(
+            BufferedWriter bWrite = new BufferedWriter(new FileWriter(fileName));
+        ){
+            for(String entry:data){
+                bWrite.write(entry+delimiter);
+                bWrite.newLine();
+            }
+        }catch(IOException e){
+           System.out.println("Unexpected IOException has been thrown");
+        }
+    }
+    public static void main(String[]args){
+        //try{
+            try {
+                File myObj = new File("test.txt");
+                if (myObj.createNewFile()) {
+                  System.out.println("File created: " + myObj.getName());
+                } else {
+                  System.out.println("File already exists.");
+                }
+              } catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+              }
+           // File file = new File("test.txt");
+            Scanner in = new Scanner(System.in);
+            SNIDDb test = new SNIDDb("test.txt",',');
+            for(int i = 0;i<5;i++){
+                String[] hi = {"User",Integer.toString(i)};
+                test.putNext(hi);
+               // test.reWrite();
+            }
+            
+
+
+        //}catch(Exception e){
+            // TODO Implement error handling
+            System.out.println("Testing");
+       // }
+    }
 }
