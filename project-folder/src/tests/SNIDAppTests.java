32a2c555dbadfa9379c2bb2eabff753f939b1f88
Jason <jasonblue1626@gmail.com>
2020-05-07 17:29:58 -0500

    Added unit tests for search by id

--- project-folder/src/tests/SNIDAppTests.java
+++ project-folder/src/tests/SNIDAppTests.java
@@ -4,17 +4,51 @@ import data.*;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
+import org.junit.Before;
+
 import app.*;
+
 public class SNIDAppTests {
-    
-    SNIDApp test = new SNIDApp("test.txt",',');
-    @Test
-    public void test_getFather(){
-        test.registerBirth('M',1000,"Jason", "Andre", "Gayle");
-        test.registerBirth('M',999,"Steve","Something","Jobs");
-        test.registerBirth('F',420,"Martha","Something","Gates");
+    SNIDApp test;
+    @Before
+    public void init(){
+        test = new SNIDApp("test.txt",',');
+        test.registerBirth('M',1000,"Bob", "Andre", "Stevens");
+        test.registerBirth('M',999,"Steve","Something","Stevens");
+        test.registerBirth('F',420,"Martha","Something","Stevens");
         test.addParentData("1", "2", "3");
+    }
+   /* @Test
+    public void test_getMother(){
+        assertEquals("3,Martha,Something,Stevens",test.getMother("1"));
+    } */
+    @Test
+    public void test_search(){
+        SNIDApp test2 = new SNIDApp("test.txt",',');
+        test.registerBirth('M',1000,"Bob", "Andre", "Stevens");
+        test.registerBirth('M',999,"Steve","Something","Stevens");
+        test.registerBirth('F',420,"Martha","Something","Stevens");
+        assertEquals("1,M,Bob,Andre,Stevens",test.search("1"));
+        assertEquals("2,M,Steve,Something,Stevens",test.search("2"));
+        assertEquals("3,F,Martha,Something,Stevens",test.search("3"));
+    }
+
+    @Test
+    public void test_getParents(){
         String father = test.getFather("1");
-        assertEquals("2,Steve,Something,Jobs", father);
+        assertEquals("3,Martha,Something,Stevens",test.getMother("1"));
+        assertEquals("2,Steve,Something,Stevens", test.getFather("1"));
+    }
+    
+
+    public static void main(String[]args){
+        SNIDApp test = new SNIDApp("test.txt",',');
+        test.registerBirth('M',1000,"Bob", "Andre", "Stevens");
+        test.registerBirth('M',999,"Steve","Something","Stevens");
+        test.registerBirth('F',420,"Martha","Something","Stevens");
+        test.addParentData("1", "2", "3");
+        System.out.println(test.getFather("1"));
+        System.out.println(test.getMother("1"));
     }
 }
